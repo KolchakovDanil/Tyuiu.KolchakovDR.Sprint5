0d9cb86325848be4c080a23657af4f4a0766080e
KolchakovDanil <danyakisliy@mail.ru>
2023-11-26 21:01:22 +0500

    Task7

---
+++ Tyuiu.KolchakovDR.Sprint5.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KolchakovDR.Sprint5.Task7.V24.Lib;
+using System.IO;
+
+namespace Tyuiu.KolchakovDR.Sprint5.Task7.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFileOutPut()
+        {
+            string path = @"C:\Users\d4six\source\repos\Tyuiu.KolchakovDR.Sprint5\Tyuiu.KolchakovDR.Sprint5.Task7.V24\bin\Debug\OutPutDataFileTask7V24.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+        [TestMethod]
+        public void CheckedExistsFileInPut()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask7V24.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KolchakovDR.Sprint5.Task7.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KolchakovDR.Sprint5.Task7.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KolchakovDR.Sprint5.Task7.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d0d35af2-bf11-48d9-bebc-fa11fd7744bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
